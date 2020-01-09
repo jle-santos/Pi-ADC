445eceee8459467fa422ac67ee43e5b82f2155aa
jle-santos <santos.lemuel@outlook.com>
2020-01-08 16:35:17 -0800

    Added Pi code

---
+++ adcPi.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import spidev
+import time
+
+# Definitions 
+ch = 1 # Channel 0
+
+# SPI Initialization 
+spi = spidev.SpiDev()
+spi.open(0,0) 
+spi.max_speed_hz=1000000
+
+# Function: Read a channel
+# Arg1 - Channel to read (1-8)
+def readADC(channel):
+	# channel = channel - 1
+	if channel > 7 or channel < 0:
+		return -1
+	rawData = spi.xfer2([1, 8 + channel << 4, 0])
+	ADCData = ((rawData[1] & 3) << 8) + rawData[2]
+	return ADCData
+	
+count = 0
+
+while True:
+	data = []
+	
+	for channel in range(8):
+		data.append(readADC(channel))
+	
+	print(data)
+	print("\n")
